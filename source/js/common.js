fe7e1d4db8a9fbfa4e25391fd4d04936d487a96a
romanshliakhov <romanshliakhov@gmail.com>
2021-08-01 12:15:00 +0300

    update photo

--- source/js/common.js
+++ source/js/common.js
@@ -93,9 +93,9 @@ $(function () {
     }
   });
 
-  $("a[href^='#']").click(function(){
+  $("a[href^=#up]").click(function(){
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
-});
+  });
 });
